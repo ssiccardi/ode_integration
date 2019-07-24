3f63c917e53b3ca04aab97e071b63f70ec2301e9
Luca_Ravezzani <ssiccardi@2ssas.it>
2019-07-24 12:40:42 +0200

    Started program to integrate a large system with a sliding window

--- slidingequations.py
+++ slidingequations.py
@@ -1,4 +1,10 @@
-# First Example of usage of the odeint scipy function
+# Integrate a system of many equation, each representing a physical element,
+#   using a window of active elements. That is: we apply an input to the first element
+#   and integrate some elements after it. When the 1st element stops changing, we
+#   delete it from the set of equations and add another element after the last one, and so on
+# LIMITATIONS:
+# ONLY constant input function = 1 at first cell is considered
+# NO cycling (=closed bundles) are supported
 
 import numpy as np
 from scipy.integrate import odeint
@@ -9,13 +15,15 @@ import random as rng
 # basic parameters
 b = 0.02
 k=10**9
-neqs = 64  # does not succeed with 400 eqs 
+neqs = 64  # windows of equations considered
+toteqs = 256 # total number of equations
 fine = 5000
-durata = 5000
+durata = 5000  # must be = fine, only constant input
 #electrodes = [0,1,2] # to test with an external potential at the first electrodes
 electrodes = [0]
 newinit = 0  # to restart integration
-cycling = 0  # if =1 the bundle is cloed on itslef
+cycling = 0  # no closed bundles
+starteq = 0  # first equation to integrate
 
 # single filament
 L = 1.7*10**-12
@@ -41,12 +49,17 @@ print(k*L*C0)
 print(1/(k*np.sqrt(L*C0)))
 
 # function that returns dv/dt etc.
-# we have 3 second order equations, so 6 first order
-# and a variable vector [v1, v2, v3, dv1dt, dv2dt, dv3dt]
+# we have neqs second order equations, so 2*neqs first order
+# and a variable vector [v1, v2, v3, ..., dv1dt, dv2dt, dv3dt, ...]
 def model1(t,xy):
+# total number of equations = toteqs
+# equations actually integrated = startqs to starteq+neqs-1
+# equations 0 to starteq = const = initial value; equations starteq+neqs = const = 0
+# this function considers only the neqs to integrate, the calling one slides them
+# TODO: how to manage non constant input functions?
     for i in range(neqs):
         if 1 -2*b*xy[i] == 0:
-            raise Exception('V%s causes division by zero (=1/2b) at t = %s' % (i,t))
+            raise Exception('V%s causes division by zero (=1/2b) at t = %s' % (i+starteq,t))
     res = np.zeros(2*neqs)
     res[neqs]= (1/((k**2)*L*C0*(1 -2*b*xy[0])))*(L*C0 * 2*b*(k*xy[neqs])**2 + ufunc(t,0) + cycling*xy[neqs-1] + xy[1] - 2*xy[0] - R1*C0 *k*(xy[neqs] - 2*b*xy[0]*xy[neqs]) - R2*C0*k*(2*(xy[neqs] - 2*b*xy[0]*xy[neqs])-(xy[neqs+1] - 2*b*xy[1]*xy[neqs+1])-cycling*(xy[2*neqs-1] - 2*b*xy[neqs-1]*xy[2*neqs-1])))
     for i in range(1,neqs-1):
@@ -55,108 +68,51 @@ def model1(t,xy):
     res[2*neqs-1]= (1/((k**2)*L*C0*(1 -2*b*xy[neqs-1])))*(L*C0 * 2*b*(k*xy[2*neqs-1])**2 + ufunc(t,neqs-1) +cycling*xy[0] + xy[neqs-2] - 2*xy[neqs-1] - R1*C0 *k*(xy[2*neqs-1] - 2*b*xy[neqs-1]*xy[2*neqs-1]) - R2*C0*k*(2*(xy[2*neqs-1] - 2*b*xy[neqs-1]*xy[2*neqs-1])-(xy[2*neqs-2] - 2*b*xy[neqs-2]*xy[2*neqs-2])-cycling*(xy[neqs] - 2*b*xy[0]*xy[neqs])))
     for i in range(neqs):
         res[i] = xy[i+neqs]
+
+#        res = np.zeros(2*toteqs)
+#    res[toteqs+starteq]= (1/((k**2)*L*C0*(1 -2*b*xy[starteq])))*(L*C0 * 2*b*(k*xy[toteqs+starteq])**2 + ufunc(t,0) + cycling*xy[neqs-1] + xy[1] - 2*xy[0] - R1*C0 *k*(xy[toteqs+starteq] - 2*b*xy[starteq]*xy[toteqs+starteq]) - R2*C0*k*(2*(xy[toteqs+starteq] - 2*b*xy[0]*xy[toteqs+starteq])-(xy[toteqs+starteq+1] - 2*b*xy[1+starteq]*xy[toteqs+starteq+1])-cycling*(xy[2*neqs-1] - 2*b*xy[neqs-1]*xy[2*neqs-1])))
+#    for i in range(1,neqs-1):
+#        res[toteqs+starteq+i]= (1/((k**2)*L*C0*(1 -2*b*xy[i+starteq])))*(L*C0 * 2*b*(k*xy[toteqs+starteq+i])**2 + ufunc(t,i) + xy[i+starteq-1] + xy[i+starteq+1] - 2*xy[i+starteq] - R1*C0 *k*(xy[toteqs+starteq+i] - 2*b*xy[i+starteq]*xy[toteqs+starteq+i]) - R2*C0*k*(2*(xy[toteqs+starteq+i] - 2*b*xy[i+starteq]*xy[toteqs+starteq+i])-(xy[toteqs+starteq+i+1] - 2*b*xy[i+starteq+1]*xy[toteqs+starteq+i+1])-(xy[toteqs+starteq+i-1] - 2*b*xy[i+starteq-1]*xy[toteqs+starteq+i-1])))
+    
+#    res[toteqs+starteq+neqs-1]= (1/((k**2)*L*C0*(1 -2*b*xy[starteq+neqs-1])))*(L*C0 * 2*b*(k*xy[toteqs+starteq+neqs-1])**2 + ufunc(t,starteq+neqs-1) +cycling*xy[0] + xy[starteq+neqs-2] - 2*xy[starteq+neqs-1] - R1*C0 *k*(xy[toteqs+starteq-1] - 2*b*xy[neqs+starteq-1]*xy[toteqs+starteq-1]) - R2*C0*k*(2*(xy[toteqs+starteq-1] - 2*b*xy[neqs+starteq-1]*xy[toteqs+starteq-1])-(xy[toteqs+starteq-2] - 2*b*xy[neqs+starteq-2]*xy[toteqs+starteq-2])-cycling*(xy[neqs] - 2*b*xy[0]*xy[neqs])))
+#    for i in range(neqs):
+#        res[i+starteq] = xy[i+toteqs+starteq]
+#        
+#    for i in range(starteq):
+#        res[i] = x[i][len(x[i])-1] # const = last value reached
+#        res[toteqs+i] = 0
+#    # the following instr are usless: it's already all zeros
+#    #for i in range(starteq+neqs,toteqs):
+#    #    res[i] = 0    # const = 0 not yet reached by the excitation
+#    #    res[toteqs+i] = 0 
+
+
     return res
     
 def ufunc(t,i):
+#    i=k+starteq
+# at the beginning this is the stimulus
+# after this is the value of the discarded element, that is = 1
     if i not in electrodes:
         return 0
     tt = t+newinit
-    tt0 = durata/10
-    if tt<durata:
-#        return np.cos(np.pi*t)
-        if i==1:
-            xx=1.0
-        else:
-            xx=0.5
-        #return 0.00006*xx*np.sin((t+newinit)/1000)  # to compare to the Mathematica old progr
-#        return np.cos((1/(k*np.sqrt(L*C0)*0.7))*tt)  # RLC resonance?
-#        if i==0:
-#            return np.cos(2*np.pi*tt/(durata/2))
-#        else:
-#            return -np.cos(2*np.pi*tt/(durata/2))
-        tmp=(tt-tt0)/tt0
-#        return (1/2 - ((np.exp(tmp)-np.exp(-tmp))/(2*(np.exp(tmp)+np.exp(-tmp)))))
-#        if i==0:
-#            return (((np.exp(tmp)-np.exp(-tmp))/((np.exp(tmp)+np.exp(-tmp)))))
-#        else:
-#            return -(((np.exp(tmp)-np.exp(-tmp))/((np.exp(tmp)+np.exp(-tmp)))))
-        if i==0:  
-            return 1
-        else:
-            return -1
+    if tt<=durata:
+        return 1
     elif t<durata*2:
-#        return np.cos((1/(k*np.sqrt(L*C0)*0.7))*tt)*np.exp(-0.5*(t-durata)**2) # RLC resonance?
-#        return np.cos(2*np.pi*tt/(durata/2))*np.exp(-0.5*(tt-durata)**2)
         return np.exp(-0.5*(tt-durata)**2)
-#        return 0
     else:
         return 0
 
 
 
-#n=fine*50+1
 t1=[0]
 x=[[0],[0],[0]]  
-#x=[[1],[1],[1]]  # to compare to the old mathematica program with in. cond 1
-for i in range(3,neqs):
-    x.append([0])
-#for i in range(20,40):
-#    x[i][0]=1
-#print(x)
-# to compare old programs with the "initial bump"
-#x[0][0] = 0.05*0.01
-#x[1][0] = 0.05*0.025
-#x[2][0] = 0.05*0.075
-#x[3][0] = 0.05*0.15
-#x[4][0] = 0.05*0.275
-#x[5][0] = 0.05*0.4
-#x[6][0] = 0.05*0.4
-#x[7][0] = 0.05*0.275
-#x[8][0] = 0.05*0.15
-#x[9][0] = 0.05*0.075
-#x[10][0] = 0.05*0.025
-#x[10][0] = 0.05*0.01
-xy=np.zeros(neqs*2)
-# set all = 0 to test comparison to old progr with sin(x) added
-#xy[0] = 1
-#xy[1] = 1
-#xy[2] = 1
-#xy[0+neqs] = -1
-#xy[1+neqs] = -1
-#xy[2+neqs] = -1
-# to compare old programs with the "initial bump"
-#xy[0] = 0.5*0.01
-#xy[1] = 0.5*0.025
-#xy[2] = 0.5*0.075
-#xy[3] = 0.5*0.15
-#xy[4] = 0.5*0.275
-#xy[5] = 0.5*0.4
-#xy[6] = 0.5*0.4
-#xy[7] = 0.5*0.275
-#xy[8] = 0.5*0.15
-#xy[9] = 0.5*0.075
-#xy[10] = 0.5*0.025
-#xy[11] = 0.5*0.01
-#xy[0+neqs] = -0.5*0.01
-#xy[1+neqs] = -0.5*0.015
-#xy[2+neqs] = -0.5*0.05
-#xy[3+neqs] = -0.5*0.075
-#xy[4+neqs] = -0.5*0.125
-#xy[5+neqs] = -0.5*0.125
-#xy[6+neqs] = 0
-#xy[7+neqs] = 0.5*0.125
-#xy[8+neqs] = 0.5*0.125
-#xy[9+neqs] = 0.5*0.075
-#xy[10+neqs] = 0.5*0.05
-#xy[11+neqs] = 0.5*0.015
-#xy[12+neqs] = 0.5*0.01
 
-#for i in range(20,40):
-#    xy[i]=1
-#xy[2] = 1
+for i in range(3,toteqs):
+    x.append([0])
+xy=np.zeros(neqs*2)
 
 r = ode(model1).set_integrator('lsoda', method='bdf',nsteps=5000)
-#r = ode(model1).set_integrator('vode', method='bdf',nsteps=5000)
 r.set_initial_value(xy, 0)
 dt=0.1
 stepi=0
@@ -166,16 +122,35 @@ maxval = -99999
 minval = 99999
 timemax = -1   # time of max value
 timemin = -1   # time of min value
+stopcount = 0
+iterations = 0
+
 while r.successful() and r.t < fine:
     t1.append(r.t+dt)
     sol=r.integrate(r.t+dt)
     stepi=stepi+1
     if stepi % 20 == 0:
         print("step %s" % stepi)
-    for i in range(neqs):
-#        print(x[i])
-#        print(sol[i])
+    for i in range(starteq):
+        x[i].append(x[i][len(x[i])-1])
+        
+    for i in range(starteq,neqs):
         x[i].append(sol[i])
+    for i in range(neqs,toteqs):
+        x[i].append(0)
+    # check if first cell is constant
+    if np.absolute(sol[0] - 1) < 0.05:
+        stopcount = stopcount + 1
+    else:
+        stopcount = 0
+    if stopcount > 3:
+        if starteq<toteqs-1:
+            starteq = starteq + 1
+        else:
+            iterations = 10 # DON'T go on computing!
+            break
+        stopcount = 0
+    # the following evaluations should be modified!
     if sol[neqs-1] >= 0.0025 and time80 == -1:
         time80 = r.t
     if sol[0] <= 0.05 and time05 == -1:
@@ -190,9 +165,9 @@ while r.successful() and r.t < fine:
 # if it does not succeed, I try restarting with initial conditions = the last values
 #   computed
 
-iterations = 0
 
 print("Time now %s" % r.t)
+print("starteq %s" % starteq)
 
 while iterations<5:
     newinit = newinit+r.t
@@ -205,6 +180,7 @@ while iterations<5:
         xy[i+neqs]= (x[i][len(x[i])-1]-x[i][len(x[i])-2])/dt
     r.set_initial_value(xy,0)
     remain = fine-newinit
+    stopcount = 0
     print("Time remaining to integrate %s" % remain)
     while r.successful() and r.t < remain:
         t1.append(newinit+r.t+dt)
@@ -212,10 +188,25 @@ while iterations<5:
         stepi=stepi+1
         if stepi % 20 == 0:
             print("step %s" % stepi)
-        for i in range(neqs):
-#        print(x[i])
-#        print(sol[i])
+        for i in range(starteq):
+            x[i].append(x[i][len(x[i])-1])
+        for i in range(starteq,neqs):
             x[i].append(sol[i])
+        for i in range(neqs,toteqs):
+            x[i].append(0)
+        # check if first cell is constant
+        if np.absolute(sol[0] - 1) < 0.05:
+            stopcount = stopcount + 1
+        else:
+            stopcount = 0
+        if stopcount > 3:
+            if starteq < toteqs-1:
+                starteq = starteq + 1
+            else:
+                iterations = 10
+                break
+            stopcount = 0
+        # the following evaluations should be modified!
         if sol[neqs-1] >= 0.8 and time80 == -1:
             time80 = r.t
         if sol[0] <= 0.05 and time05 == -1:
@@ -242,20 +233,14 @@ if electrodes:
     plt.show()
 
 
-for i in range(min(neqs,10)):
+for i in range(min(toteqs,10)):
     color = '#'+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))
     plt.plot(t1,x[i],color,linewidth=2,label='v'+str(i))
 
-#plt.plot(t1,x[0],'b-',linewidth=2,label='v1')
-#plt.plot(t1,x[1],'r-',linewidth=2,label='v2')
-#plt.plot(t1,x[2],'g-',linewidth=2,label='v2')
-#plt.plot(t1,x[3],'b--',linewidth=2,label='v2')
-#plt.plot(t1,x[4],'r--',linewidth=2,label='v2')
-#plt.plot(t1,x[5],'g--',linewidth=2,label='v2')
 
 plt.legend()
 plt.show()
-if neqs>=20:
+if toteqs>=20:
     for i in range(10,20):
         color = '#'+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))
         plt.plot(t1,x[i],color,linewidth=2,label='v'+str(i))
@@ -263,7 +248,7 @@ if neqs>=20:
 
 plt.legend()
 plt.show()
-if neqs>=30:
+if toteqs>=30:
     for i in range(20,30):
         color = '#'+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))
         plt.plot(t1,x[i],color,linewidth=2,label='v'+str(i))
@@ -272,7 +257,7 @@ if neqs>=30:
     plt.legend()
     plt.show()
 
-if neqs>=40:
+if toteqs>=40:
     for i in range(30,40):
         color = '#'+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))
         plt.plot(t1,x[i],color,linewidth=2,label='v'+str(i))
@@ -281,7 +266,7 @@ if neqs>=40:
     plt.legend()
     plt.show()
 
-if neqs>=64:
+if toteqs>=64:
     for i in range(54,63):
         color = '#'+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))
         plt.plot(t1,x[i],color,linewidth=2,label='v'+str(i))
@@ -290,7 +275,7 @@ if neqs>=64:
     plt.legend()
     plt.show()
 
-if neqs>=370:
+if toteqs>=370:
     for i in range(360,370):
         color = '#'+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))
         plt.plot(t1,x[i],color,linewidth=2,label='v'+str(i))
@@ -299,14 +284,26 @@ if neqs>=370:
     plt.legend()
     plt.show()
 
-if neqs>=10:
-    for i in range(neqs-10,neqs):
+if toteqs>=10:
+    for i in range(toteqs-10,toteqs):
         color = '#'+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))
         plt.plot(t1,x[i],color,linewidth=2,label='v'+str(i))
 
 
     plt.legend()
     plt.show()
+
+if toteqs>=16:
+    for i in range(0,toteqs,16):
+        color = '#'+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))
+        plt.plot(t1,x[i],color,linewidth=2,label='v'+str(i))
+
+    color = '#'+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))+'{:02x}'.format(rng.randint(0,255))
+    plt.plot(t1,x[toteqs-1],color,linewidth=2,label='v'+str(toteqs-1))
+
+    plt.legend()
+    plt.show()
+
 
 print("Last element reached max %s at %s" % (maxval,timemax))
 print("First element dropped at min %s at %s" % (minval,timemin))
